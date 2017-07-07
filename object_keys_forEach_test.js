6a2954132acf1a4b1f5e10c758b5f53a8dfd8197
Kevin Hwang <hjs6877@gmail.com>
2017-07-07 14:57:29 +0900

    Initial commit

---
+++ object_keys_forEach_test.js
@@ -0,0 +1,14 @@
+/**
+ * Created by kjs on 2017-07-03.
+ */
+var webrtc_discussions = {
+    "aaaa" : {
+        0 : true,
+        1 : true,
+        2 : true
+    }
+};
+
+Object.keys(webrtc_discussions["aaaa"]).forEach(function(id){
+    console.log(id);
+});
